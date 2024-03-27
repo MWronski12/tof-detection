df97d8227e861bba860dc3a92697bda3923f5de5
Mikołaj Wroński <wronski.mikolaj@gmail.com>
2024-03-27 21:01:42 +0100

    Plot data from client run

--- client.py
+++ client.py
@@ -1,36 +1,60 @@
-#!/usr/bin/python3
-
 import socket
 import struct
 import time
+import sys
+import signal
+from matplotlib import pyplot as plt
+from typing import List, Tuple
+import numpy as np
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "192.168.185.1"  # The server's hostname or IP address
 PORT = 8080  # The port used by the server
 
-MAX_DIST_MM = 3400
-plot_data = []
+data: List[Tuple[int, int]] = []
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
 
-    t_start = time.time()
-    while True:
-        data = s.recv(4)
-        if not data:
-            print("Connection closed!")
-            break
+def signal_handler(signal, frame):
+    plot_data()
+    sys.exit(0)
 
-        # Convert the received data from network byte order to host byte order
-        dist_mm = struct.unpack("!i", data)[0]
 
-        if len(plot_data) > 1:
-            plot_data.append((time.time() - t_start, dist_mm))
-        else:
-            plot_data.append((time.time() - t_start, dist_mm))
+def plot_data():
+    # Extract x and y coordinates from the list of tuples
+    x_values = [point[0] for point in data]
+    y_values = [point[1] for point in data]
 
-        if len(plot_data) == 100:
-            print("======= PLOT DATA =======")
-            for i in range(len(plot_data)):
-                print("{:3.5}".format(plot_data[i][0]) + f" : {plot_data[i][1]}")
-            print("=========================")
-            plot_data = []
+    # Plot the points
+    plt.scatter(x_values, y_values, color="blue")
+    plt.title("Scatter Plot of Points")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid(True)
+
+    plt.show()
+
+
+def do_collect_data() -> None:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print("Successfully connected to data stream!")
+
+        t_start = time.time()
+        while True:
+            dist_mm_bytes = s.recv(4)
+            if not dist_mm_bytes:
+                print("Connection closed!")
+                break
+
+            # Convert the received data from network byte order to host byte order
+            dist_mm = struct.unpack("!i", dist_mm_bytes)[0]
+            print(dist_mm)
+            data.append((time.time() - t_start, dist_mm))
+
+
+def main() -> None:
+    signal.signal(signal.SIGINT, signal_handler)
+    do_collect_data()
+
+
+if __name__ == "__main__":
+    main()
