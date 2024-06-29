8d5f8e6886ecbedfe29a1793199184177ef032fd
Mikołaj Wroński <wronski.mikolaj@gmail.com>
2024-06-29 11:26:23 +0200

    Add --headless argument to compile script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,18 @@
+if [ $# -gt 2 ]; then
+    echo "Usage: $0 optional[--headless]"
+    exit 1
+fi
+
+if [ "$1" = "--headless" ]; then
+    echo "Compiling headless version"
+    main="headless_main.c"
+else
+    echo "Compiling server version"
+    main="main.c"
+fi
+
 gcc -Werror -Wall -pedantic \
     -I driver/include \
     -o build/main \
     -O0 \
-    data_collector.h data_collector.c measurements.c measurements.h main.c
+    data_collector.h data_collector.c measurements.c measurements.h ${main}
